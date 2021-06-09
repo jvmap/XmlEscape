37b0ff4807b96a3fe11324933635393430ea7e15
Jeroen Vrijkorte <vrijkorte@posthuma-partners.nl>
2021-06-09 09:51:44 +0200

    Initial version of xml escape utility.

---
+++ XmlEscape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace XmlEscape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                PrintUsage();
+            else if (args[0] == "--help" || args[0] == "-?")
+                PrintUsage();
+            else
+            {
+                string path = args[0];
+                using Stream input =
+                    File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                XmlEscaper.Escape(input, Console.Out);
+            }
+        }
+
+        static void PrintUsage()
+        {
+            string cmd = Environment.GetCommandLineArgs()[0];
+            Console.WriteLine($"Usage: {cmd} <fileName>");
+            Console.WriteLine("Prints escaped XML to stdout.");
+        }
+    }
+}
---
+++ XmlEscape/XmlEscaper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlEscape
+{
+    class XmlEscaper
+    {
+        public static void Escape(Stream input, TextWriter output)
+        {
+            Encoding encoding = GetEncoding(input);
+            using var reader = new StreamReader(input, encoding);
+            string unescaped = reader.ReadToEnd();
+            string escaped = SecurityElement.Escape(unescaped);
+            output.Write(escaped);
+        }
+
+        private static Encoding GetEncoding(Stream input)
+        {
+            var reader = new XmlTextReader(input);
+            reader.MoveToContent();
+            input.Position = 0;
+            return reader.Encoding;
+            // don't Dispose the reader, because doing so closes the input stream.
+        }
+    }
+}
